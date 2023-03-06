603f8643092e11bf2351480fe97382d46fc12f77
Kenny Su <kfernandosuqiu@gmail.com>
2023-03-06 18:16:58 -0500

    Email - Fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,10 +24,10 @@ const Contact = () => {
     } else {
       emailjs
         .send(
-          "service_seruhwu", // service id
-          "template_21aw58z", // template id
+          "service_57lj603", // service id
+          "template_nzxux2h", // template id
           mailData,
-          "Q3pccdLZhU-mZT7tQ" // public api
+          "oplActhvGASgXhAdP" // public api
         )
         .then(
           (response) => {
